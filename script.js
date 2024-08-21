736369b71c288ac16b66f0289e579b7f065ce9df
Mohammed Shamshaddin <mohammedshamshaddin@gmail.com>
2024-08-20 22:10:33 -0400

    JavaScript Lab1

---
+++ script.js
@@ -0,0 +1,23 @@
+let firstName = "Mohammed";
+let lastName = "Shamshaddin";
+let fullName = firstName + " " + lastName;
+let population = 1443000;
+let javaScriptIsCool = true;
+const colors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"];
+
+function JS(){
+    console.log(fullName);
+    console.log(population);
+    console.log(population *3);
+
+    for (i=1; i<6; i++){
+        console.log(i);
+    }
+
+    for (i=0; i<5; i++){
+        console.log(i);
+    }
+
+}
+
+JS();
